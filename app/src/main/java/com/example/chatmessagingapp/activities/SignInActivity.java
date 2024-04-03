3f99c6f50c3fc41f747702899394b41ca6fdecb4
Mithun Param <paramatm@mcmaster.ca>
2024-04-02 20:36:52 -0400

    setting up project

--- app/src/main/java/com/example/chatmessagingapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatmessagingapp/activities/SignInActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.chatmessagingapp.R;
+
 public class SignInActivity extends AppCompatActivity {
 
     @Override
