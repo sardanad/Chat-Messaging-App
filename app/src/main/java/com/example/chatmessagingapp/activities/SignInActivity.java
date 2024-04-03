abe01147f7ba36cc4a417863537508756fc9dc0b
Mithun Param <paramatm@mcmaster.ca>
2024-04-02 21:48:06 -0400

    set up sign in and sign up page layouts

--- app/src/main/java/com/example/chatmessagingapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatmessagingapp/activities/SignInActivity.java
@@ -2,15 +2,27 @@ package com.example.chatmessagingapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.chatmessagingapp.R;
+import com.example.chatmessagingapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
---
+++ app/src/main/java/com/example/chatmessagingapp/activities/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.example.chatmessagingapp.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatmessagingapp.R;
+import com.example.chatmessagingapp.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
