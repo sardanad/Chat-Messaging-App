873365e7f1a73739aee48fa7ef5e9231afa87287
sardanad <sardanad@mcmaster.ca>
2024-04-07 14:26:54 -0400

    Error Ommission Retrofit

--- app/src/main/java/com/example/chatmessagingapp/network/ApiClient.java
+++ app/src/main/java/com/example/chatmessagingapp/network/ApiClient.java
@@ -1,7 +1,7 @@
 package com.example.chatmessagingapp.network;
 
 import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
@@ -9,7 +9,7 @@ public class ApiClient {
 
     public static Retrofit getClient(){
         if(retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl("https://fcm.googleapis.com/fcm/").addConverterFactory(ScalarsConverterFactory.create()).build();
+            retrofit = new Retrofit.Builder().baseUrl("https://fcm.googleapis.com/fcm/").addConverterFactory(GsonConverterFactory.create()).build();
         }
 
         return retrofit;
